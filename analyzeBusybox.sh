230f591f7110d7987358dbd91f70cb2cc5224b80
Jörg Liebig <joliebig@fim.uni-passau.de>
2013-02-19 18:54:13 +0100

    minor fixes of project setup

--- analyzeBusybox.sh
+++ analyzeBusybox.sh
@@ -1,16 +1,30 @@
 #!/bin/bash -e
 #!/bin/bash -vxe
 
+SKIP=1
+NTH=1
+
+if [ $# -eq 2 ]
+then
+	SKIP=$1
+	NTH=$2
+fi
+
 filesToProcess() {
   local listFile=busybox/busybox_files
-  cat $listFile
-  #awk -F: '$1 ~ /.c$/ {print gensub(/\.c$/, "", "", $1)}' < linux_2.6.33.3_pcs.txt
+  sed -n "$SKIP~${NTH}p" $listFile
 }
 
 
 flags="-U HAVE_LIBDMALLOC -DCONFIG_FIND -U CONFIG_FEATURE_WGET_LONG_OPTIONS -U ENABLE_NC_110_COMPAT -U CONFIG_EXTRA_COMPAT -D_GNU_SOURCE"
 srcPath="busybox-1.18.5"
-export partialPreprocFlags="-x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelFExpr busybox/featureModel --debugInterface --writePI --recordTiming --parserstatistics"
+export partialPreprocFlags="-x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelFExpr busybox/featureModel --debugInterface --writePI --recordTiming --parserstatistics --typecheck"
+
+## recompile TypeChef
+#cd ../TypeChef
+#java -jar sbt-launch.jar mkrun
+#cd -
+
 ## Reset output
 filesToProcess|while read i; do
   if [ ! -f $srcPath/$i.dbg ]; then
